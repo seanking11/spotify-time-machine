482bd4278397c36cc4d9e296afb960f9924fc8c9
Sean King <kingsc@miamioh.edu>
2018-02-01 19:58:17 -0500

    build: Add antd as dependency and update webpack loaders

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import promise from 'redux-promise'
+import promiseMiddleware from 'redux-promise'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 
 import App from './components/app'
 import Builder from './components/Builder'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(promiseMiddleware)))
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={App} />
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
     }]
   },
   resolve: {
